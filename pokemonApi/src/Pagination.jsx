efc06ebd033888a324dde58e344f796a4134b752
InesMedem <inesmedem93@gmail.com>
2024-03-28 11:53:29 +0100

    done

--- pokemonApi/src/Pagination.jsx
+++ pokemonApi/src/Pagination.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
-const Pagination = () => {
-  return <div></div>;
+const Pagination = ({ gotoNextPage, gotoPrevPage }) => {
+  return (
+    <div>
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
+      {gotoNextPage && <button onAuxClick={gotoNextPage}>Next</button>}
+    </div>
+  );
 };
 
 export default Pagination;
