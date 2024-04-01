18e41a64d21c3a6ca5e1a32b0e2e15126ad49151
InesMedem <inesmedem93@gmail.com>
2024-04-01 14:53:00 +0200

    like button

--- pokemonApi/src/components/LikeButton.jsx
+++ pokemonApi/src/components/LikeButton.jsx
@@ -1,8 +1,17 @@
 const LikeButton = ({ isLiked, onToggleLike }) => {
   return (
-    <>
-      <button onClick={onToggleLike}>{isLiked ? "Unlike" : "Like"}</button>
-    </>
+    <button onClick={onToggleLike}>
+      {isLiked ? (
+        <span className="material-symbols-outlined text-red-500">favorite</span>
+      ) : (
+        <span
+          className="material-symbols-outlined text-red-500"
+          style={{ fontVariationSettings: "'FILL' 1" }}
+        >
+          favorite
+        </span>
+      )}
+    </button>
   );
 };
 
